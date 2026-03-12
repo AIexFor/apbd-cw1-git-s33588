0012858216ca70db2bf759bc158ec7b88b8ab274
s33588 <s33588@pjwstk.edu.pl>
2026-03-12 15:06:38 +0100

    Add base func

---
+++ Test.py
@@ -0,0 +1,11 @@
+def is_primal(number):
+    if number < 2:
+        return False
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
+
+print(is_primal(3))
